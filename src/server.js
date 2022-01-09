5cb74935e9f91be4ac1a0eefedbcceb2dc6d2246
leejanghe <babybye777@gmail.com>
2022-01-10 00:35:57 +0900

    환경설정 최종완료

--- src/server.js
+++ src/server.js
@@ -5,8 +5,14 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/public/views");
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req,res)=>{
     res.render("home")
+})
+
+app.get("/*", (req, res)=>{
+    res.redirect("/");
 })
 
 const handleListen = () => {
